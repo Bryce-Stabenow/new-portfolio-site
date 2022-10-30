4225724a7d1a6ad47f048414962852796de7236a
Bryce Stabenow <brycestabenow617@gmail.com>
2022-10-30 13:42:13 -0700

    spinning icon

--- src/components/Quote.vue
+++ src/components/Quote.vue
@@ -1,7 +1,19 @@
 <template>
-    <div class="text-white text-3xl w-full flex flex-col flex-wrap align-center px-5 my-20">
+    <div class="text-white text-3xl w-full flex flex-col flex-wrap align-center px-5 my-10">
+        <img src="stroopwafel-solid.svg" class="h-20 invert mb-10">
       <span class="leading-snug">"Good design is obvious;</span>
       <span class="mb-4 ml-3 leading-snug"><strong>Great design is transparent."</strong></span>
       <span class="text-xl ml-3">-JOE SPARANO</span>
     </div>
 </template>
+
+<style scoped>
+img{
+    animation: spin 20s linear 0s infinite;
+}
+
+@keyframes spin {
+    0% {transform: rotate(0deg);}
+    100% {transform: rotate(360deg);}
+}
+</style>
